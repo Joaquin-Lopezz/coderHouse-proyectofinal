aa727ac268df27150e41e9c5d8a0be6eef75adc0
Joaquin Lopez <joaquin.ariel.lopez.98@gmail.com>
2024-08-06 10:48:52 -0600

    documentacion y supertest

---
+++ src/routers/api/sesiones.router.js
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { soloLogueadosApi } from '../../middlewares/autorizaciones.js';
+import passport from 'passport';
+
+export const sesionesRouter = Router();
+
+//await fetch('/api/sesiones
+sesionesRouter.post('/',passport.authenticate('loginLocal', { failWithError: true,}),
+  
+    async (req, res, next) => {
+        res.status(201).json({ status: 'success', message: 'login success' });
+    },
+    (error, req, res, next) => {
+        res.status(401).json({ status: 'error', message: error.message });
+    }
+);
+
+sesionesRouter.get('/current', soloLogueadosApi, (req, res) => {
+    res.json(req.user);
+});
+
+sesionesRouter.delete('/current', (req, res) => {
+    try {
+        req.session.destroy((err) => {
+            if (err) {
+                return res.status(500).json({ status: 'logout error', body: err });
+            }
+            res.json({ status: 'success', message: 'logout OK' });
+        });
+    } catch (error) {
+        console.log(error)
+        next(error)    
+    }
+});
+
